79ed94e316e98fccc28494a2944cd16f592071a9
Rohit45chowdhury <rohitgtr4@gmail.com>
2025-04-18 16:36:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+function hamburg(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
+}
+
+function cancel(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}
+
+
+
+  
+
+
+// Typewriter effect
+
+var typed = new Typed(".typewriter-text",
+    {
+        strings: ["DATA SCIENTIST", "GRAPHIC DESIGNER", "AIML ENGINEER"],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true
+    }
+)
+
+
+
+  
